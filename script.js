91bffc0959670987719f54b2cc1e735ad569362e
Kenny Saavedra Altuna <69105862+KSAplay@users.noreply.github.com>
2021-07-28 17:05:51 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,59 @@
+// -------------------------------------------------------------------------
+//                               VARIABLES
+// -------------------------------------------------------------------------
+// Variables auxiliares
+var peso_final = 0;
+var peso_usuario = 0;
+var planetaSeleccionado = "Saturno";
+
+// Gravedades de los planetas
+var g_mercurio = 3.7;
+var g_venus = 8.87;
+var g_tierra = 9.807;
+var g_marte = 3.721;
+var g_jupiter = 24.8;
+var g_saturno = 10.44;
+var g_urano = 8.87;
+var g_neptuno = 11.15;
+
+// -------------------------------------------------------------------------
+//                               FUNCIONES
+// -------------------------------------------------------------------------
+function cambiarImagen(){
+    planetaSeleccionado = $("select.planetas option:selected").text();
+    $("img#imagen").replaceWith('<img id="imagen" src="img/'+planetaSeleccionado+'.png" alt="'+planetaSeleccionado+'" height="38.5%" width="50%">');
+    calcularPeso();
+}
+
+function calcularPeso(){
+    // Obteniendo el peso
+    peso_usuario = $("input#pesoUsuario").val();
+    // Calculando su valor según el planeta seleccionado
+    switch(planetaSeleccionado){
+        case "Marte":
+            peso_final = peso_usuario * g_marte / g_tierra;
+            break;
+        case "Mercurio":
+            peso_final = peso_usuario * g_mercurio / g_tierra;
+            break;
+        case "Venus":
+            peso_final = peso_usuario * g_venus / g_tierra;
+            break;
+        case "Júpiter":
+            peso_final = peso_usuario * g_jupiter / g_tierra;
+            break;
+        case "Saturno":
+            peso_final = peso_usuario * g_saturno / g_tierra;
+            break;
+        case "Urano":
+            peso_final = peso_usuario * g_urano / g_tierra;
+            break;
+        case "Neptuno":
+            peso_final = peso_usuario * g_neptuno / g_tierra;
+            break;
+    }
+    // Mostrando valores
+    $("p#peso").replaceWith('<p id="peso">En la <strong>Tierra</strong> su peso es de<br><strong>'+peso_usuario+'</strong> kilos</p>');
+    $("p#pesoCalculado").replaceWith('<p id="pesoCalculado">En <strong>'+planetaSeleccionado+'</strong> su peso sería de<br><strong>'+(peso_final).toFixed(1)+'</strong> kilos</p>');
+    //console.log(peso_usuario);
+}
