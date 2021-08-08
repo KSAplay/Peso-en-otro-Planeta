7b25f79447d1802f44084a5424a6dbbf6bfbe84c
Kenny Saavedra Altuna <69105862+KSAplay@users.noreply.github.com>
2021-08-07 19:17:24 -0500

    Optimización de los inputs y ajustes a las estrellas y planetas
    Se optimizó la respuesta a la hora de ingresar el peso para que al ingresar un numero ya se haya calculado sin realizar otro click.
    Se redujo el tamaño de las estrellas del segundo y tercer grupo, y se agregó animación a los planetas.

--- script.js
+++ script.js
@@ -1,33 +1,44 @@
 // -------------------------------------------------------------------------
 //                               VARIABLES
 // -------------------------------------------------------------------------
+// Inputs
+const pesoUsuario = document.getElementById('pesoUsuario');
+pesoUsuario.addEventListener("input", calcularPeso);
+
+const planetas = document.getElementById('planetas');
+planetas.addEventListener("change", cambiarImagen);
+
+// Etiquetas que serán reemplazadas
+var imgContainer, pesoContainer, pesoCalculadoContainer;
+
 // Variables auxiliares
 var peso_final = 0;
 var peso_usuario = 0;
 var planetaSeleccionado = "Saturno";
 
 // Gravedades de los planetas
-var g_mercurio = 3.7;
-var g_venus = 8.87;
-var g_tierra = 9.807;
-var g_marte = 3.721;
-var g_jupiter = 24.8;
-var g_saturno = 10.44;
-var g_urano = 8.87;
-var g_neptuno = 11.15;
+const g_mercurio = 3.7;
+const g_venus = 8.87;
+const g_tierra = 9.807;
+const g_marte = 3.721;
+const g_jupiter = 24.8;
+const g_saturno = 10.44;
+const g_urano = 8.87;
+const g_neptuno = 11.15;
 
 // -------------------------------------------------------------------------
 //                               FUNCIONES
 // -------------------------------------------------------------------------
 function cambiarImagen(){
-    planetaSeleccionado = $("select.planetas option:selected").text();
-    $("img#imagen").replaceWith('<img id="imagen" src="img/'+planetaSeleccionado+'.png" alt="'+planetaSeleccionado+'" height="38.5%" width="50%">');
+    planetaSeleccionado = planetas.value;
+    imgContainer = document.getElementById('img-container').innerHTML = `<img id="imagen" src="img/`+planetaSeleccionado+`.png" alt="`+planetaSeleccionado+`" height="38.5%" width="50%">`;
     calcularPeso();
 }
 
 function calcularPeso(){
     // Obteniendo el peso
-    peso_usuario = $("input#pesoUsuario").val();
+    //peso_usuario = $("input#pesoUsuario").val();
+    peso_usuario = pesoUsuario.value;
     if(peso_usuario == ""){
         peso_usuario = 0.0;
     }
@@ -56,7 +67,7 @@ function calcularPeso(){
             break;
     }
     // Mostrando valores
-    $("p#peso").replaceWith('<p id="peso">En la <strong>Tierra</strong> tu peso es de<br><strong>'+peso_usuario+'</strong> kg</p>');
-    $("p#pesoCalculado").replaceWith('<p id="pesoCalculado">En <strong>'+planetaSeleccionado+'</strong> tu peso sería de<br><strong>'+(peso_final).toFixed(1)+'</strong> kg</p>');
+    pesoContainer = document.getElementById('peso-container').innerHTML = `<p id="peso">En la <strong>Tierra</strong> tu peso es de<br><strong>`+peso_usuario+`</strong> kg</p>`;
+    pesoCalculadoContainer = document.getElementById('pesoCalculado-container').innerHTML = `<p id="pesoCalculado">En <strong>`+planetaSeleccionado+`</strong> tu peso sería de<br><strong>`+(peso_final).toFixed(1)+`</strong> kg</p>`;
     //console.log(peso_usuario);
 }
